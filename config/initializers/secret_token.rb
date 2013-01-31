580b9380f167b8c4a6e4a5487b1d3b1e5feacb2e
Alexander Bazanov <bazanovam@gmail.com>
2013-01-31 22:47:25 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timepro::Application.config.secret_token = '6c7d9c8dc10bf7ac063419136400026b9ff358dffcc75d49c95aca795405e0c924cedb84f37f42eef46015836550f981d12c091ee8adff464757da0d22f73141'
