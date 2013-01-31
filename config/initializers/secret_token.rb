e51d78a841ff0e292bdc2a3b3bd7733c5076acc2
Alexander Bazanov <bazanovam@gmail.com>
2013-01-31 23:08:33 +0400

    aa

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Timepro::Application.config.secret_token = '6c7d9c8dc10bf7ac063419136400026b9ff358dffcc75d49c95aca795405e0c924cedb84f37f42eef46015836550f981d12c091ee8adff464757da0d22f73141'
+Timepro::Application.config.secret_token = '345c952f29ba7ff8bb8d9b206af03a8630741726d06e8cece1e5acb9deb637dec64956a8f1e01e0f24e1bc3f54951de3a4d6a0dacac52563c651e33028d7b675'
